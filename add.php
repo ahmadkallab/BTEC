c8e3888ed3f817d84f9e29d6d39bbdc67efefdba
ahmadkallab <39456161+ahmadkallab@users.noreply.github.com>
2024-02-09 22:32:32 +0300

    Add files via upload

---
+++ add.php
@@ -0,0 +1,31 @@
+<?php
+    $servername = "kallabbakery-server.mysql.database.azure.com";
+    $username = "dgadequuds";
+    $password = "4VUACNNA3J3502FX$";
+    $dbname = "kallabbakery-database";
+    
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: ". $conn->connect_error);
+    }
+    
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    
+        // Collect values from the form
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $message = $_POST['message'];
+        
+        // Replace with the actual column names in your 'products' table
+        $sqlquery = "INSERT INTO products (column1, column2, column3) VALUES ('$name', '$email', '$message')";
+        
+        if ($conn->query($sqlquery) == TRUE) {
+            echo "Record inserted successfully";
+        } else {
+            echo "Error: " . $sqlquery . "<br>" . $conn->error;
+        }
+    }
+?>
