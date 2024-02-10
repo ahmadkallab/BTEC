a7db6b85f89ac5588a94d7a80dc2f0ad20fb0ec3
ahmadkallab <39456161+ahmadkallab@users.noreply.github.com>
2024-02-10 22:01:50 +0300

    Add files via upload

---
+++ search.php
@@ -0,0 +1,34 @@
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "store";
+    
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: ". $conn->connect_error);
+    }
+        
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    
+        // collect value of input field
+        $searchWord = $_REQUEST['searchWord'];
+    
+        $sqlquery = "SELECT * FROM products WHERE Pname LIKE '%$searchWord%'";
+    
+        $result = $conn->query($sqlquery);
+
+        if ($result->num_rows > 0) {
+            // output data of each row
+            while($row = $result->fetch_assoc()) {
+                echo "id: " . $row["id"]. " - Name: " . $row["Pname"]. " , color: " . $row["color"]. "<br>";
+            }
+        } else {
+            echo "0 results";
+        }
+        $conn->close();
+    }
+?>
